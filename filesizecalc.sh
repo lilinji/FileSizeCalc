12650e766672c047ef4e4eefd42341bb99a94757
Shashank Srivastava <shashank.ssriv@gmail.com>
2017-09-29 19:44:25 +0530

    Create filesizecalc.sh

---
+++ filesizecalc.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+#Author : - Shashank Srivastava
+#Date : - 29th September
+set -x
+echo ""
+echo "Welcome to FileSizeCalc()! Enter your choice below in form of a digit followed by K/M/G (in caps)."
+echo ""
+echo "For example, if you want to search for files bigger than 100 MBs, enter 100M."
+echo -n "Enter your pattern... "
+read CHOICE
+#Intentionally removing space in case user adds a space before K/M/G
+SIZE=`echo $CHOICE | sed 's/ //g'`
+echo -n "Enter the filesystem. For example, /home... "
+read FS_CHOICE
+echo "Great! You chose to search for file(s) greater than $SIZE in $FS_CHOICE!"
+echo "Below are the file(s) with size greater than $SIZE in $FS_CHOICE!"
+find $FS_CHOICE -xdev -size +$SIZE -exec ls -ltrh {} \;
+echo ""
+echo "Thanks for using FileSizeCalc(). Hope to see you soon :-)"
