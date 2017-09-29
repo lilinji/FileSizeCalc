382f5353a44513cfbb1ac742eb0db830616f0e1b
Shashank Srivastava <shashank.ssriv@gmail.com>
2017-09-29 21:02:39 +0530

    added appropriate message in case no files are returned with the chose pattern.

--- filesizecalc.sh
+++ filesizecalc.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 #Author : - Shashank Srivastava
 #Date : - 29th September
-set -x
+#set -x
 echo ""
 echo "Welcome to FileSizeCalc()! Enter your choice below in form of a digit followed by K/M/G (in caps)."
 echo ""
@@ -10,10 +10,14 @@ echo -n "Enter your pattern... "
 read CHOICE
 #Intentionally removing space in case user adds a space before K/M/G
 SIZE=`echo $CHOICE | sed 's/ //g'`
-echo -n "Enter the filesystem. For example, /home... "
+echo -n "Enter the filesystem. For example, /home... /"
 read FS_CHOICE
 echo "Great! You chose to search for file(s) greater than $SIZE in $FS_CHOICE!"
 echo "Below are the file(s) with size greater than $SIZE in $FS_CHOICE!"
 find $FS_CHOICE -xdev -size +$SIZE -exec ls -ltrh {} \;
+if [ $? -eq 0 ]
+then
+  echo "Sorry, there were not any files bigger than $SIZE. Please try with a lower size."
+fi
 echo ""
 echo "Thanks for using FileSizeCalc(). Hope to see you soon :-)"
